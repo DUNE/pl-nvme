a892e2fba0264640b4a8dbb7575217630a0c276a
Terry Barnaby <terry.barnaby@beam.beam.ltd.uk>
2020-05-11 10:38:19 +0100

    Major change to GIT source tree layout, simplifying it.

--- source/DuneNvmeTest/Config.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-#	Config.mk	Makefile build system settings
-#	T.Barnaby,	Beam Ltd,	2020-02-18
-################################################################################
-#
-PROJECT		= DuneNvmeTest
-VERSION		= 0.1.1
-DOCDIR		?= ${TOP}/doc/${PROJECT}
