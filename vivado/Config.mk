3c23e35a80b6916336c5c54b07a15c610723b25a
Adam Gillard [Summer Student 2020 <ag17009@bayban.phy.bris.ac.uk>
2020-07-30 12:42:27 +0100

    Added Config.mk to /vivado

---
+++ vivado/Config.mk
@@ -0,0 +1,8 @@
+################################################################################
+#	Config.mk	DuneNvme Beam build configuration
+#			T.Barnaby,	BEAM Ltd,	2020-05-21
+################################################################################
+#
+PROJECT		?= DuneNvmeTest
+VIVADO_PATH	?= /software/CAD/Xilinx/2019.2/Vivado/2019.2/bin
+VIVADO_TARGET	?= "vivado"
