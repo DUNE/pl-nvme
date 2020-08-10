10dea6d284b7df545d6458d74d6963154b7b637a
Adam Gillard [Summer Student 2020 <ag17009@excession.phy.bris.ac.uk>
2020-08-10 10:32:57 +0100

    Source and config changes

--- vivado/Config-template.mk
+++ vivado/Config-template.mk
@@ -3,6 +3,6 @@
 #			T.Barnaby,	BEAM Ltd,	2020-05-21
 ################################################################################
 #
-PROJECT		?= DuneNvmeTestOpsero
-VIVADO_PATH	?= /opt/Xilinx/Vivado/2019.2/bin
-VIVADO_TARGET	?= ""
+PROJECT		?= DuneNvmeTest
+VIVADO_PATH	?= /software/CAD/Xilinx/2019.2/Vivado/2019.2/bin
+VIVADO_TARGET	?= "vivado"
